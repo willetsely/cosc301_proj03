22ae4812c6903bc0ab2ae61075f46197c2449caf
mgeorges <mgeorges@colgate.edu>
2013-11-06 17:55:49 -0500

    madd work being done

--- main.c
+++ main.c
@@ -9,14 +9,22 @@ void dump_memory_map(void);
 
 int main(int argc, char **argv) {
     void *m1 = malloc(50);  // should allocate 64 byte
-    void *m2 = malloc(100); // should allocate 128 bytes
-    free(m1);
+	dump_memory_map();    
+	void *m2 = malloc(100); // should allocate 128 bytes
+	dump_memory_map();    
+	myfree(m1);
     dump_memory_map();
     void *m3 = malloc(56);  // should allocate 64 bytes
-    void *m4 = malloc(11);  // should allocate 32 bytes
-    free(m3);
-    void *m5 = malloc(30);  // should allocate 64 bytes
-    void *m6 = malloc(120); // should allocate 128 bytes
-    free(m2);
-    return 0;
+	dump_memory_map();    
+	void *m4 = malloc(11);  // should allocate 32 bytes
+	dump_memory_map();    
+	myfree(m3);
+	dump_memory_map();    
+	void *m5 = malloc(30);  // should allocate 64 bytes
+	dump_memory_map();    
+	void *m6 = malloc(120); // should allocate 128 bytes
+	dump_memory_map();    
+	myfree(m2);
+	dump_memory_map();    
+	return 0;
 }
